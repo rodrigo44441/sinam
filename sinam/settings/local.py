b632036cb0577d8d2b8245e14c07b1151e41595c
rodrigo <rodrigo44441@gmail.com>
2017-10-25 01:55:51 -0300

    probando el git pull al server 2

--- sinam/settings/local.py
+++ sinam/settings/local.py
@@ -1,5 +1,10 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
